aea54c65ca762c329bdc73774e00210c6439fedf
Kofi <pastorkofi101@gmail.com>
2023-08-11 09:47:19 -0400

    Build sound section component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Jumbtron from "./components/Jumbotron";
 import Nav from "./components/Nav"
+import SoundSection from "./components/SoundSection";
 function App() {
 
   return (
     <div className="App">
       <Nav />
       <Jumbtron />
+      <SoundSection />
     </div>
   );
 }
--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,6 +3,16 @@ import IPhone from "../assets/images/iphone-14.jpg"
 import HoldingIPhone from "../assets/images/iphone-hand.png"
 
 function Jumbtron() {
+
+    const handleLearnMore = () => {
+        const element = document.querySelector(".sound-section");
+        window.scrollTo({
+            top: element?.getBoundingClientRect().top,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
     return (  
         <div className="jumbotron-section wrapper">
             <h2 className='title'>New</h2>
@@ -13,7 +23,7 @@ function Jumbtron() {
             </span>
             <ul className='links'>
                 <li><button className='button'>Buy</button></li>
-                <li><a className='link'>Learn more</a></li>
+                <li><a className='link' onClick={handleLearnMore}>Learn more</a></li>
             </ul>
             <img className='iphone-img' src={HoldingIPhone} alt="Person holding iPhone"/>
         </div>
